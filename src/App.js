636fe93371dc36a6a166a26924829ea273bbef38
Arpit Anand <arpitssanand@gmail.com>
2021-11-30 21:19:04 +0530

    Code Refactor

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import SwapTable from "./Components/SwapTable/SwapTable";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <SwapTable />
-      </header>
+    <div className="container">
+      <div className="app">
+      <h1 className="heading">Table Assignment</h1>
+          <SwapTable />
+      </div>
     </div>
   );
 }
--- src/Components/SwapTable/SwapTable.js
+++ src/Components/SwapTable/SwapTable.js
@@ -1,18 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 // Table Components
 import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
-// Icons
-import { IconButton } from "@mui/material";
-import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
-import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+// Components
+import TableHeadComponent from "./TableHeadComponent";
+import TableBodyComponents from "./TableBodyComponent";
 
 function createData(_id, firstName, lastName, email) {
   return { _id, firstName, lastName, email };
@@ -29,68 +24,11 @@ const rows = [
 export default function DenseTable() {
   const [data, setData] = useState(rows);
 
-  const handelChange = (rowData, upFlag) => {
-    let dataChange = data;
-    let currentDataIndex = dataChange.findIndex((data) => data === rowData);
-    if (upFlag) {
-      let temp = dataChange[currentDataIndex];
-      dataChange[currentDataIndex] = dataChange[currentDataIndex - 1];
-      dataChange[currentDataIndex - 1] = temp;
-    } else {
-      let temp = dataChange[currentDataIndex];
-      dataChange[currentDataIndex] = dataChange[currentDataIndex + 1];
-      dataChange[currentDataIndex + 1] = temp;
-    }
-    setData([...dataChange]);
-  };
-
-  const dataRow = () => {
-    return data.map((row, index) => (
-      <TableRow
-        key={row._id}
-        sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-      >
-        <TableCell component="th" scope="row">
-          {!(index === 0) && (
-            <IconButton
-              color="secondary"
-              aria-label="add an alarm"
-              onClick={() => handelChange(row, true)}
-            >
-              <ArrowUpwardIcon />
-            </IconButton>
-          )}
-        </TableCell>
-        <TableCell align="right">{row.firstName}</TableCell>
-        <TableCell align="right">{row.lastName}</TableCell>
-        <TableCell align="right">{row.email}</TableCell>
-        <TableCell align="right">
-          {!(index === data.length - 1) && (
-            <IconButton
-              color="secondary"
-              aria-label="add an alarm"
-              onClick={() => handelChange(row, false)}
-            >
-              <ArrowDownwardIcon />
-            </IconButton>
-          )}
-        </TableCell>
-      </TableRow>
-    ));
-  };
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell> </TableCell>
-            <TableCell align="right">First Name</TableCell>
-            <TableCell align="right">Last Name</TableCell>
-            <TableCell align="right">Email</TableCell>
-            <TableCell align="right"> </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>{dataRow()}</TableBody>
+        <TableHeadComponent />
+        <TableBodyComponents data={data} setData={setData} />
       </Table>
     </TableContainer>
   );
